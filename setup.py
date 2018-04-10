24300fc5b60261095eff4782de7f368aa22bd54d
eeiwant <wangyue@clickwise.cn>
2018-04-10 11:31:52 +0800

    1.rename pyenc/ py source file to pyx file. 2.edit setup.py add cython extension

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ Copyright NadiaF0rever
 '''
 
 from distutils.core import setup
+from Cython.Build import cythonize
 
 setup(
         name = "pyenc",
@@ -13,5 +14,6 @@ setup(
         author = "eeiwant",
         author_email = "eeiwant@gmail.com",
         scripts = ["bin/pyenc"],
+        ext_modules = cythonize("pyenc/*.pyx"),
         packages = ["pyenc"]
     )
